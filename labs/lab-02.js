dbb395b136ae0071a0cae444712bcf74e178e458
John Paxton <pax@speedingplanet.com>
2021-08-30 15:38:01 -0400

    Lab 2

--- labs/lab-02.js
+++ labs/lab-02.js
@@ -1,41 +1,42 @@
-function getMean(...values) {
+function getMean( ...values ) {
   return (
     values.reduce(
-      (accumulatedValue, current) => accumulatedValue + current,
-      0
+      ( accumulatedValue, current ) => accumulatedValue + current,
+      0,
     ) / values.length
   );
 }
 
-const getMedian = (...values) => {
-  return values.sort((a, b) => b - a)[2];
+const getMedian = ( ...values ) => {
+  return values.sort( ( a, b ) => b - a )[Math.round( values.length / 2 )];
 };
 
-const getMode = function (...values) {
-  let count = {};
-  values.forEach((v) => (count[v] = count[v] ? count[v] + 1 : 1));
+const getMode = function( ...values ) {
+  const count = {};
+  values.forEach( ( v ) => ( count[v] = count[v] ? count[v] + 1 : 1 ) );
 
   let most = [];
 
-  for (let entry of Object.entries(count)) {
-    let arg = entry[0];
-    let value = entry[1];
+  for ( const entry of Object.entries( count ) ) {
+    const arg = entry[0];
+    const value = entry[1];
 
-    if (most.length === 0 || count[most[0]] === value) {
-      most.push(arg);
-    } else if (value > count[most[0]]) {
-      most = [arg];
+    if ( most.length === 0 || count[most[0]] === value ) {
+      most.push( arg );
+    } else if ( value > count[most[0]] ) {
+      most = [ arg ];
     }
   }
 
   return most;
 };
 
-function calculateStats(...values) {
-  return [getMean(...values), getMedian(...values), getMode(...values)];
+// getMean(1, 5, 32, 8, 4)
+function calculateStats( ...values ) {
+  return [ getMean( ...values ), getMedian( ...values ), getMode( ...values ) ];
 }
 
-let [mean, median, mode] = calculateStats(
+const [ mean, median, mode ] = calculateStats(
   3,
   6,
   2,
@@ -48,7 +49,7 @@ let [mean, median, mode] = calculateStats(
   3,
   3,
   7,
-  4
+  4,
 );
 
-console.log(`Mean ${mean} | Median ${median} | Mode: ${mode}`);
+console.log( `Mean ${mean} | Median ${median} | Mode: ${mode}` );
